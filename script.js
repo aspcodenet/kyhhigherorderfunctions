1d920700b8a70f2c141521b38e8a0232fae2d233
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-03-10 13:00:27 +0100

    2

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 import { countries } from "./data.js";
 
 console.log(countries.length)
+
+function addTal(){
+    return 12+11;
+}
+
+const result = addTal()
+
+//Ta fram alla countries som har population > 450 000 000
+let filtered = []
+for(let i = 0; i < countries.length; i++){
+    const country =  countries[i];
+    if (country.population > 450000000){
+        filtered.push(country)
+    }
+}
+
+console.log(filtered)
